9e576a110d9677c97dfa17aebe26d47c63201e71
Andrew Kim <andrew_kim3@brown.edu>
2025-11-27 23:48:23 -0500

    initialize project

---
+++ WhoTexted/WhoTexted/WhoTextedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoTextedApp.swift
+//  WhoTexted
+//
+//  Created by Andrew Kim on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WhoTextedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
